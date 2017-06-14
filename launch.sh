c9c2b3bcf5126b661c2705f144606b41f2cb403c
Satyam Zode <satyamzode@gmail.com>
2017-06-14 12:00:45 +0530

    Add support to use env vars
    This will allow user to inject github repo link
    through GIT_REPO env variable

--- launch.sh
+++ launch.sh
@@ -5,4 +5,4 @@ echo "Fetching notebooks from" $GIT_REPO
 git clone $GIT_REPO /home/notebooks
 
 echo "Running Notebook"
-/opt/conda/bin/jupyter notebook --ip='*' --notebook-dir=/home/notebooks --port=8888 --no-browser
+/opt/conda/bin/jupyter notebook --ip='*' --notebook-dir=/home --port=8888 --no-browser
