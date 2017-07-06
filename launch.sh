6bd8436f7873370e5cc8ad108c5aeb8e3eb5d9fd
Harsh Vardhan <harsh59v@gmail.com>
2017-07-06 18:05:07 +0530

    Adds auto-commit and file download facility

--- launch.sh
+++ launch.sh
@@ -3,6 +3,8 @@
 echo "Fetching notebooks from" $GIT_REPO
 
 git clone $GIT_REPO /home/notebooks
+cp jupyter_notebook_config.py /home/notebooks/
+cp openebs_utils.py /home/notebooks/
 
 echo "Running Notebook"
 /opt/conda/bin/jupyter notebook --ip='*' --notebook-dir=/home --port=8888 --no-browser
